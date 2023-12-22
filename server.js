416c5219d56525a3390c30331ae744859ce20f42
Dolly Aggarwal <dollyaggarwal1712@gmail.com>
2023-12-22 11:45:00 +0530

    implemented swagger documention of APIs.

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 import express from 'express';
+import swagger, {serve} from 'swagger-ui-express';
+
+import apiDocs from './swagger.json' assert {type: 'json'};
+import jwtAuth from './src/middlewares/jwt.middleware.js';
 import { ApplicationError } from './error-handler/applicationError.js';
 import loggerMiddleware from './src/middlewares/logger.middleware.js';
 import userRouter from './src/features/user/user.routes.js';
 import postRouter from './src/features/posts/post.routes.js';
 import commentRouter from './src/features/comments/comment.routes.js';
 import likeRouter from './src/features/likes/like.routes.js';
-import jwtAuth from './src/middlewares/jwt.middleware.js';
+import { type } from 'os';
+
 
 const app = express();
 
 app.use(express.json());
-app.use(loggerMiddleware);  
+app.use('/api-docs', swagger.serve, swagger.setup(apiDocs));
+app.use(loggerMiddleware);
+
 app.use('/api/users',userRouter);
 app.use('/api/posts',jwtAuth,postRouter);
 app.use('/api/comments',jwtAuth,commentRouter);
@@ -18,10 +25,6 @@ app.use('/api/likes',jwtAuth,likeRouter);
 
 app.get('/',(req, res) =>{
     res.send('Welcome To Social Networking APIs');
-});
-
-app.use((req, res) =>{
-    res.status(404).send("API not found!..Please check our documentation for more information at localhost:5500/api-docs")
 });
 
 app.use((err, req, res, next)=>{
@@ -32,7 +35,9 @@ app.use((err, req, res, next)=>{
     res.status(500).send('Something went wrong , Please try later.');
 });
 
-
+app.use((req, res) =>{
+    res.status(404).send("API not found!..Please check our documentation for more information at localhost:5500/api-docs")
+});
 
 app.listen(5000,()=>{
     console.log("Server is running at port 5000");
--- src/features/comments/comment.routes.js
+++ src/features/comments/comment.routes.js
@@ -5,7 +5,7 @@ const commentRouter = express.Router();
 const commentController = new CommentController();
 
 commentRouter.post('/addcomment/:id', commentController.addComment);
-commentRouter.get('/:id', commentController.getComment);
+commentRouter.get('/:id',commentController.getComment);
 commentRouter.delete('/delete/:id',commentController.deleteComment);
 commentRouter.put('/update/:id',commentController.updateComment);
 
